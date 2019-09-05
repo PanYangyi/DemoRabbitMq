f365f54fbadaa7d906054b9f907e1d4aa2986fc3
PanYangyi <1052149997@qq.com>
2019-09-05 11:15:21 +0800

    修改new api

--- src/main/java/com/rabiitmq/demo/helloworld/Recv.java
+++ src/main/java/com/rabiitmq/demo/helloworld/Recv.java
@@ -1,8 +1,6 @@
 package com.rabiitmq.demo.helloworld;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.*;
 import com.rabiitmq.demo.ConnectionUtil;
 
 import java.io.IOException;
@@ -22,7 +20,26 @@ public class Recv {
         Channel channel = connection.createChannel();
         //声明队列
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        //定义队列的消费者
+
+        //----------new api-----------------------------
+        //定义消费者
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel){
+            //获取到发送的消息
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msg = new String(body,"utf-8");
+                System.out.println("new api recv : "+msg);
+
+            }
+        };
+
+        //监听队列
+        channel.basicConsume(QUEUE_NAME,true,defaultConsumer);
+
+
+        //----------old api----------------------------
+
+        /*//定义队列的消费者
         QueueingConsumer consumer = new QueueingConsumer(channel);
         //监听队列
         channel.basicConsume(QUEUE_NAME, true, consumer);
@@ -31,7 +48,7 @@ public class Recv {
             QueueingConsumer.Delivery delivery = consumer.nextDelivery();
             String message = new String(delivery.getBody());
             System.out.println(" [x] Received '" + message + "'");
-        }
+        }*/
 
 
     }
